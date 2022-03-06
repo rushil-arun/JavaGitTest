9b0eaf22515ee5e0bf49945a20a891b699170fcb
rushil-arun <85460707+rushil-arun@users.noreply.github.com>
2022-03-06 14:10:59 -0800

    added a new class

---
+++ src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    private int x;
+    private int y;
+    public App(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int multiply(){
+        return this.x * this.y;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main{
     public static void main (String[] args){
         System.out.println("Hi");
+        System.out.println(add(5, 3));
+        App a = new App(5, 3);
+        System.out.println(a.multiply());
+    }
+
+    public static int add(int a, int b){
+        return a + b;
     }
 }
